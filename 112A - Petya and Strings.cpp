a808863c9595231e8b0315ed294ab5097bd7c163
William Jefferson <80225184+williamjefferson6@users.noreply.github.com>
2024-02-02 19:06:27 +0600

    Create 112A - Petya and Strings.cpp

---
+++ 112A - Petya and Strings.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    for (int i = 0; i < s1.length(); i++) {
+        if (s1[i] < 92) {
+            s1[i] += 32;
+        }
+        if (s2[i] < 92) {
+            s2[i] += 32;
+        }
+    }
+    if (s1 < s2) {
+        cout << -1;
+    }
+    else if (s1 > s2) {
+        cout << 1;
+    }
+    else if (s1 == s2){
+        cout << 0;
+    }
+}
